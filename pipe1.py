24c4cb711bfe85bb315718a7cd013a32abfb581a
Irene Chang <41071119h@gapps.ntnu.edu.tw>
2025-03-03 11:35:20 +0800

    First try

--- pipe1.py
+++ pipe1.py
@@ -2,6 +2,6 @@ from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
 
-res = classifier("I've been waiting for a HuggingFace course my whole life.")
+res = classifier("好爽窩")
 
 print(res)
